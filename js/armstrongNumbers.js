9e970fe1beac12149d10b7fdd7f6c80fb1810762
rvillarreal <richard.villarreal2@hotmail.com>
2025-11-02 15:39:17 -0600

    Completed both JS and Python

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,19 @@
 // How can you make this more scalable and reusable later?
+exports.findArmstrongNumbers = function(numbers) {
+    armstrongs = [];
 
-exports.findArmstrongNumbers = function() {
+    for(let num of numbers){
+        const numStr = num.toString()
+        const digits = numStr.split('').map(digit=> parseInt(digit))
+        const power = digits.length
 
+        let sum = 0
+        for (let digit of digits){
+            sum += Math.pow(digit, power)       
+        }
+         if (sum === num){
+            armstrongs.push(num)
+        }
+    }
+    return armstrongs
 };
--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,15 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    armstrongs = []
+    
+    for num in numbers:
+        num_list = list(map(int, str(num)))
+        power = len(num_list)
+        sum = 0
+        for digit in num_list:
+            sum += digit ** power  
+        if sum == num:
+            armstrongs.append(num)
+            
+    return armstrongs       
+
